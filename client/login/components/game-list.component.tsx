b198a9d308e57a6f5839b36ee203401f9713c5f3
ychaika <ychaika@corevalue.net>
2018-05-22 16:17:06 +0300

    login-game-request

--- client/login/components/game-list.component.tsx
+++ client/login/components/game-list.component.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as constants from '../../../constants.json';
 
 export interface GameListProps {
     gameList: Array<string>,
@@ -12,13 +13,21 @@ export interface GameListProps {
  * @classdesc Renders a list of created games.
  * */
 export class GameListComponent extends React.Component<GameListProps, {}> {
+    private apiHost: string;
+
+    constructor(props) {
+        super(props);
+
+        this.apiHost = `${constants['appUrl']}:${constants['appPortHttp']}`;
+    }
 
     render() {
         return <div className={'login-form'}>
             <h3>Select a Game</h3>
             <div className={'login-form__games-list'}>
                 {[...this.props.gameList].map((gameName, i) => {
-                    return <div className={'login-form__game'} key={i}>{gameName}</div>
+                    return <div className={'login-form__game'} key={i}
+                                onClick={() => this.getGame({gameName})}>{gameName}</div>
                 })}
             </div>
             <input className={'login-form__input'} type="text" placeholder="Enter name of new game" value={this.props.newGameName} onChange={(e) => {
@@ -29,6 +38,14 @@ export class GameListComponent extends React.Component<GameListProps, {}> {
             }}>New Game
             </button>
         </div>
+    }
+
+
+    public getGame (name): Promise<any> {
+        name = name['gameName'].replace(' ', '-');
+        return fetch(`${this.apiHost}/games/:${name}`)
+            .then((res) => console.log(res))
+            .catch((error) => console.log(error));
     }
 
 }
